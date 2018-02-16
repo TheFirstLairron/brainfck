bf43ce6b39ee3ce1db79c96b78df8b7a91d381fc
ozyx <jessemazzella@gmail.com>
2018-02-15 23:38:10 -0800

    add tokenize tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+mod tests {
+
+// TODO: Extract to its own file
+#[derive(PartialEq)]
+#[derive(Debug)]
 enum Token {
     LeftShift,
     RightShift,
@@ -9,10 +14,61 @@ enum Token {
     LeftLoop,
 }
 
+// TODO: Extract to proper file
+fn tokenize(token: &str) -> Token {
+    match token {
+        "<" => Token::LeftShift,
+        ">" => Token::RightShift,
+        "+" => Token::Increment,
+        "-" => Token::Decrement,
+        "," => Token::Read,
+        "." => Token::Write,
+        "[" => Token::RightLoop,
+        "]" => Token::LeftLoop,
+
+        // Is there something better to return as default?
+        _ => Token::LeftShift
+    }
+}
+
 #[cfg(test)]
-mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn tokenize_leftshift(){
+        assert_eq!(tokenize("<"), Token::LeftShift);
+    }
+    
+    #[test]
+    fn tokenize_rightshift(){
+        assert_eq!(tokenize(">"), Token::RightShift);
+    }
+
+    #[test]
+    fn tokenize_increment(){
+        assert_eq!(tokenize("+"), Token::Increment);
+    }
+
+    #[test]
+    fn tokenize_decrement(){
+        assert_eq!(tokenize("-"), Token::Decrement);
+    }
+
+    #[test]
+    fn tokenize_read(){
+        assert_eq!(tokenize(","), Token::Read);
+    }
+
+    #[test]
+    fn tokenize_write(){
+        assert_eq!(tokenize("."), Token::Write);
+    }
+
+    #[test]
+    fn tokenize_rightloop(){
+        assert_eq!(tokenize("["), Token::RightLoop);
+    }
+
+    #[test]
+    fn tokenize_leftloop(){
+        assert_eq!(tokenize("]"), Token::LeftLoop);
     }
 }
